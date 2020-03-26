d28cb9380379bdfd5fb3a62da73900b6f397140b
Ivan Ray Altomera <irayspacii@gmail.com>
2020-03-26 18:15:35 +0800

    initial commit

---
+++ lib/esc_pos_usb.dart
@@ -0,0 +1,4 @@
+library esc_pos_usb;
+
+export './src/enums.dart';
+export './src/printer_usb_manager.dart';
---
+++ lib/src/printer_usb_manager.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+import 'dart:async';
+import 'package:esc_pos_usb/src/enums.dart';
+import 'package:esc_pos_utils/esc_pos_utils.dart';
+import 'package:escposprinter/escposprinter.dart';
+
+class PrinterUsbManager {
+  bool _isPrinting = false;
+
+  Future<List> getDevices() async {
+    List devices = await Escposprinter.getUSBDeviceList;
+    return devices;
+  }
+
+  Future<void> connectDevice(int vendor, int product) async {
+    await Escposprinter.connectPrinter(vendor, product);
+    return;
+  }
+
+  Future<PosPrintResult> writeBytes(
+    List<int> bytes, {
+    int chunkSizeBytes = 20,
+    int queueSleepTimeMs = 20,
+  }) async {
+    final Completer<PosPrintResult> completer = Completer();
+
+    if (_isPrinting) {
+      return Future<PosPrintResult>.value(PosPrintResult.printInProgress);
+    }
+
+    final len = bytes.length;
+    List<List<int>> chunks = [];
+
+    for (var i = 0; i < len; i += chunkSizeBytes) {
+      var end = (i + chunkSizeBytes < len) ? i + chunkSizeBytes : len;
+      chunks.add(bytes.sublist(i, end));
+    }
+
+    for (var i = 0; i < chunks.length; i += 1) {
+      await Escposprinter.printBytes(bytes);
+      sleep(Duration(milliseconds: queueSleepTimeMs));
+    }
+
+    completer.complete(PosPrintResult.success);
+
+    return completer.future;
+  }
+
+  Future<PosPrintResult> printTicket(Ticket ticket) {
+    if (ticket == null || ticket.bytes.isEmpty) {
+      return Future<PosPrintResult>.value(PosPrintResult.ticketEmpty);
+    }
+    return writeBytes(ticket.bytes);
+  }
+}
